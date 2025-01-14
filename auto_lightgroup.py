10446dfd4a58cf12c86d52e339f0c99abb89af61
RolandVyens <leiersai@sina.com>
2025-01-14 23:27:18 +0800

    Create auto_lightgroup.py

---
+++ auto_lightgroup.py
@@ -0,0 +1,20 @@
+import bpy
+
+def auto_lightgroup():
+    light_list=[]
+    # Iterate through all collections in the current Blender scene
+    for lc in bpy.context.view_layer.layer_collection.children:
+        # Collection object
+        collection = lc.collection
+        
+        # Filter collections based on name
+        if 'lgt_' in collection.name and lc.exclude is False and lc.hide_viewport is False:
+            for obj in collection.all_objects:
+                if obj.type == 'LIGHT':
+                    light_list.append(obj.name)
+                    bpy.ops.scene.view_layer_add_lightgroup(name=f'{obj.name}')
+                    obj.lightgroup=obj.name
+    print(light_list)
+
+    return {"FINISHED"}
+        
