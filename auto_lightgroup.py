7fd53f028ba309e13f928eae01cb39a061948d90
RolandVyens <leiersai@sina.com>
2025-01-16 00:38:05 +0800

    Update auto_lightgroup.py

--- auto_lightgroup.py
+++ auto_lightgroup.py
@@ -7,7 +7,6 @@ def auto_lightgroup():
     for lc in bpy.context.view_layer.layer_collection.children:
         # Collection object
         collection = lc.collection
-
         # Filter collections based on name
         if (
             "lgt_" in collection.name
@@ -21,4 +20,45 @@ def auto_lightgroup():
                     obj.lightgroup = obj.name
     print(light_list)
 
-    return {"FINISHED"}
+    return light_list
+
+
+def auto_lightaov():
+    view_layer = bpy.context.view_layer
+    lightgroups = [lg.name for lg in view_layer.lightgroups]
+    light_objects = [
+        obj for obj in bpy.context.view_layer.objects if obj.type == "LIGHT"
+    ]
+    real_lights = []
+    for light in light_objects:
+        if light.lightgroup in lightgroups:
+            real_lights.append(light.lightgroup)
+            light.lightgroup = str()
+    if real_lights:
+        bpy.ops.scene.view_layer_remove_unused_lightgroups()
+    else:
+        return {"finished"}
+    real_lights = list(set(real_lights))
+    lightdict = {}
+    visible_set = []
+    for light in real_lights:
+        lightobj = bpy.context.view_layer.objects.get(light)
+        visible_set = [
+            lightobj.visible_diffuse,
+            lightobj.visible_glossy,
+            lightobj.visible_transmission,
+            lightobj.visible_volume_scatter,
+        ]
+        lightdict[light] = visible_set
+        visible_set = []
+        if lightdict[light][0] is True:
+            bpy.ops.scene.view_layer_add_lightgroup(name=f"diffuse_{light}")
+        if lightdict[light][1] is True:
+            bpy.ops.scene.view_layer_add_lightgroup(name=f"specular_{light}")
+        if lightdict[light][2] is True:
+            bpy.ops.scene.view_layer_add_lightgroup(name=f"transmission_{light}")
+        if lightdict[light][3] is True:
+            bpy.ops.scene.view_layer_add_lightgroup(name=f"volume_{light}")
+    # print(lightdict)
+
+    return {"finished"}
