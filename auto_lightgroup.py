3b13439aee451e10f8763b39cc7fa391eb888de4
RolandVyens <leiersai@sina.com>
2025-01-18 00:30:07 +0800

    Update auto_lightgroup.py

--- auto_lightgroup.py
+++ auto_lightgroup.py
@@ -91,3 +91,53 @@ def auto_assignlight():
         obj.hide_render = True
 
     return {"finished"}
+
+
+def auto_assignlight_scene():
+    lightgroup_dict = {}
+    lightcollection_dict = {}
+    light_dict = {}
+    for viewlayer in bpy.context.scene.view_layers:
+        if viewlayer.name[:7] != "-_-exP_" and "_DATA" not in viewlayer.name:
+            lightgroups = []
+            for lightgroup in viewlayer.lightgroups:
+                lightgroups.append(lightgroup.name)
+            lightgroup_dict[viewlayer.name] = lightgroups
+        for collection in viewlayer.layer_collection.children:
+            light_collection = []
+            if collection.name.startswith("lgt_") and collection.exclude == False:
+                light_collection.append(collection.name)
+                lightcollection_dict[viewlayer.name] = light_collection
+                lights = []
+                for object in bpy.data.collections[collection.name].all_objects:
+                    if object.type == "LIGHT":
+                        lights.append(object.name)
+                light_dict[viewlayer.name] = lights
+    temp_light = []
+    for key in lightgroup_dict.keys():
+        for lightgroup in lightgroup_dict[key]:
+            for lobe in ["diffuse_", "specular_", "transmission_", "volume_"]:
+                if lightgroup.startswith(f"{lobe}"):
+                    light = lightgroup.removeprefix(f"{lobe}")
+                    for light_object_name in light_dict[key]:
+                        light_object=bpy.context.scene.objects.get(light_object_name)
+                        if (
+                            light_object.name == light
+                            or light_object.name[:-4] == light
+                        ):
+                            obj = bpy.data.objects.get(light_object.name)
+                            duplicate = obj.copy()
+                            duplicate.data = obj.data.copy()
+                            bpy.data.collections[
+                                lightcollection_dict[key][0]
+                            ].objects.link(duplicate)
+                            duplicate.name = f"{lobe}{light}"
+                            duplicate.lightgroup = lightgroup
+                            temp_light.append(light)
+    for light in temp_light:
+        obj = bpy.data.objects.get(light)
+        obj.hide_render = True
+
+    print(lightgroup_dict)
+    print(lightcollection_dict)
+    print(light_dict)
