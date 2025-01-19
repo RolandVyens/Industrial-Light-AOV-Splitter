bacc8c50b562b7b706b1750a368df4dd1424a2c2
RolandVyens <leiersai@sina.com>
2025-01-19 18:58:27 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,13 +6,31 @@ bl_info = {
     "location": "Viewlayer tab in properties panel.",
     "description": "Auto generate outputs for advanced compositing.",
     "category": "Render",
-    "doc_url": "https://github.com/RolandVyens/Industrial-AOV-Connector",
-    "tracker_url": "https://github.com/RolandVyens/Industrial-AOV-Connector/issues",
+    "doc_url": "https://github.com/RolandVyens/Light-AOV-Splitter",
+    "tracker_url": "https://github.com/RolandVyens/Light-AOV-Splitter",
 }
 
 import bpy
+from bpy.types import Operator, AddonPreferences
+from bpy.props import BoolProperty
 from .auto_lightgroup import auto_lightgroup, auto_lightaov
 from .auto_aov_renderscript import auto_assignlight_scene, auto_restorelight_scene
+
+
+class LAS_AddonPrefs(AddonPreferences):
+    # this must match the add-on name, use '__package__'
+    # when defining this in a submodule of a python package.
+    bl_idname = __package__
+
+    UI_Show_In_Comp: BoolProperty(
+        name="Show UI In N Panel",
+        description="If enabled, show UI in N panel in addition to the property panel",
+        default=False,
+    )  # type: ignore
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "UI_Show_In_Comp")
 
 
 class LAS_OT_InitAOV(bpy.types.Operator):
@@ -24,15 +42,94 @@ class LAS_OT_InitAOV(bpy.types.Operator):
     def execute(self, context):
         auto_lightgroup()
         auto_lightaov()
-        self.report({"INFO"}, bpy.app.translations.pgettext("Created For Current Viewlayer"))
+        self.report(
+            {"INFO"}, bpy.app.translations.pgettext("Created For Current Viewlayer")
+        )
 
         return {"FINISHED"}
-        
+
 
 class LAS_PT_oPanel_Base:
 
     def draw(self, context):
-        layout=self.layout
+        layout = self.layout
         col = layout.column()
         col.scale_y = 3
         col.operator(LAS_OT_InitAOV.bl_idname, icon="OUTLINER_OB_LIGHT")
+
+
+class LAS_PT_oPanel(bpy.types.Panel, LAS_PT_oPanel_Base):
+    bl_label = "Industrial Light Splitter"
+    bl_idname = "RENDER_PT_industrialsplitter"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "view_layer"
+    bl_order = 1
+
+
+class LAS_PT_oPanel_COMP(bpy.types.Panel, LAS_PT_oPanel_Base):
+    bl_label = "Industrial Light Splitter"
+    bl_idname = "COMP_PT_industrialsplitter"
+    bl_space_type = "NODE_EDITOR"
+    bl_region_type = "UI"
+    bl_category = "Industrial"
+
+    @classmethod
+    def poll(cls, context):
+        preferences = bpy.context.preferences
+        addon_prefs = preferences.addons[__package__].preferences
+        # Ensure the panel only shows when in the compositor editor
+        return (
+            context.space_data.tree_type == "CompositorNodeTree"
+            and addon_prefs.UI_Show_In_Comp is True
+        )
+
+
+class LAS_PT_oPanel_N(bpy.types.Panel, LAS_PT_oPanel_Base):
+    bl_label = "Industrial Light Splitter"
+    bl_idname = "3DVIEW_PT_industrialsplitter"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Industrial"
+
+    @classmethod
+    def poll(cls, context):
+        preferences = bpy.context.preferences
+        addon_prefs = preferences.addons[__package__].preferences
+        # Ensure the panel only shows when in the compositor editor
+        return (
+            context.space_data.tree_type == "CompositorNodeTree"
+            and addon_prefs.UI_Show_In_Comp is True
+        )
+
+
+"""以下为注册函数"""
+reg_clss = [
+    LAS_AddonPrefs,
+    LAS_OT_InitAOV,
+    LAS_PT_oPanel,
+    LAS_PT_oPanel_COMP,
+    LAS_PT_oPanel_N,
+]
+
+
+def register():
+    for i in reg_clss:
+        bpy.utils.register_class(i)
+    # bpy.app.translations.register(__package__, language_dict)
+    bpy.app.handlers.render_init.append(auto_assignlight_scene)
+    bpy.app.handlers.render_cancel.append(auto_restorelight_scene)
+    bpy.app.handlers.render_complete.append(auto_restorelight_scene)
+
+
+def unregister():
+    for i in reg_clss:
+        bpy.utils.unregister_class(i)
+    # bpy.app.translations.unregister(__package__)
+    bpy.app.handlers.render_init.remove(auto_assignlight_scene)
+    bpy.app.handlers.render_cancel.remove(auto_restorelight_scene)
+    bpy.app.handlers.render_complete.remove(auto_restorelight_scene)
+
+
+if __name__ == "__main__":
+    register()
